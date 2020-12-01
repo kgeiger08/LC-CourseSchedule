604ddba5414361720fb805414710e28738bf8f88
kgeiger08 <72526597+kgeiger08@users.noreply.github.com>
2020-11-30 20:07:47 -0600

    Course class defined.

--- src/app/course.ts
+++ src/app/course.ts
@@ -1,2 +1,18 @@
 export class Course {
+    public title: string;
+    public code: string;
+    public days: string[];
+    public startTime: string;
+    public endTime: string;
+    public credits: number;
+
+    constructor(title: string, code: string, days: string[], startTime: string, endTime: string, credits: number) {
+        this.title = title;
+        this.code = code;
+        this.days = days;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.credits = credits;
+
+    }
 }
