a6b2f3fbd1483c2de263b4df23f4a4c376677915
kgeiger08 <72526597+kgeiger08@users.noreply.github.com>
2020-11-30 19:38:34 -0600

    set up complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { HeaderComponent } from './header/header.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScheduleComponent,
+    HeaderComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule
